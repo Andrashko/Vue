db5dd2240168e4c3afe9c6c99c2a99185ac006b5
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2020-10-30 14:08:42 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,89 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bookvie">
+    <h2>{{ book.Title }}</h2>
+    <p class="authors">{{ book.Author }}</p>
+    <p>{{ book.Price.toFixed(2) }}</p>
+    <img alt="Cover" :src="book.Cover" class="cover" />
+
+    <div v-if="book.Disount > 0" class="discount">
+      Знижка {{ book.Disount }}%
+    </div>
+    <br />
+    Рейтинг
+    <div class="stars">
+      <div v-if="book.Stars > 0" class="star">*</div>
+      <div v-if="book.Stars > 1" class="star">*</div>
+      <div v-if="book.Stars > 2" class="star">*</div>
+      <div v-if="book.Stars > 3" class="star">*</div>
+      <div v-if="book.Stars > 4" class="star">*</div>
+    </div>
+
+    <br />
+    <p v-on:click="showHideComments">Коментарі</p>
+    <div v-if="showComments">
+      <ul v-if="book.Comments.length>0">
+        <li v-for="comment in book.Comments" :key="comment">{{ comment }}</li>
+      </ul>
+      <p v-else> коментів нема </p>
+      <form>
+        <input type="text" placeholder="Новий коментар" v-model="newComment" />
+        <input type="button" value="Додати" v-on:click="addComment" />
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      newComment: "",
+      showComments: true,
+      book: {
+        Stars: 3,
+        Disount: 10,
+        Title: "Кобзар",
+        Author: "Т.Г. Шевченко",
+        Price: 450.0,
+        Cover:
+          "https://img.yakaboo.ua/media/catalog/product/cache/1/image/398x565/234c7c011ba026e66d29567e1be1d1f7/1/8/18291_26236_13.jpg",
+        Comments: ["Найкраща книга", "Дуже патріотично", "Супер"],
+      },
+    };
+  },
+  methods: {
+    addComment() {
+      this.book.Comments.push(this.newComment);
+    },
+    showHideComments(){
+      this.showComments = !this.showComments
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.authors {
+  font: 12px grey;
+}
+.bookvie {
+  background: burlywood;
+  width: 200px;
+  position: relative;
+}
+.discount {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  background: chartreuse;
+}
+.star {
+  float: left;
+}
+.cover {
+  position: relative;
+  width: 100px;
+  height: 150px;
 }
 </style>
